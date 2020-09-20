06fa6e711587efc0184a83041b53ef2f148757d5
Michael Murray <cloud19912003@live.com>
2020-09-20 18:35:31 +0000

    Automatically backed up by Learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -1,1 +1,16 @@
-# Write your code here.
+require 'pry'
+
+def line(deli_counter)
+  if deli_counter.size == 3 
+    puts "The line is currently: 1. #{deli_counter[0]} 2. #{deli_counter[1]} 3. #{deli_counter[2]}"
+  elsif deli_counter.size == 10 
+    puts "The line is currently: 1. #{deli_counter[0]} 2. #{deli_counter[1]} 3. #{deli_counter[2]} 4. #{deli_counter[3]} 5. #{deli_counter[4]} 6. #{deli_counter[5]} 7. #{deli_counter[6]} 8. #{deli_counter[7]} 9. #{deli_counter[8]} 10. #{deli_counter[9]}"
+  else
+    puts "The line is currently empty."
+  end
+end
+
+def take_a_number(deli_counter, name)
+  deli_counter << name
+  deli_counter.each.with_index(1){|name, index| puts "Welcome, #{name}. You are number #{index} in line."}
+end
