a914bb03683b85dbd9e3d059451ce845af64dcb6
Michael Murray <cloud19912003@live.com>
2020-09-20 18:54:28 +0000

    Automatically backed up by Learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -11,7 +11,6 @@ def line(deli_counter)
 end
 
 def take_a_number(deli_counter, name)
-  deli_counter = []
   deli_counter << name
-  deli_counter.each{|name| puts "Welcome, #{name}. You are number 1 in line."}
+  deli_counter.each.with_index(1){|name, index| puts "Welcome, #{name}. You are number #{index} in line."}
 end
